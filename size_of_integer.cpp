316793fd9d12b233a733a8ac81142244b694f052
吴翱翔 <pylint@yandex.com>
2020-08-26 21:22:56 +0800

    下定决心深入研究C++

---
+++ size_of_integer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    using namespace std;
+    cout << "Size of bool: " << sizeof(bool) << endl;
+    cout << "Size of char: " << sizeof(char) << endl;
+    cout << "Size of short: " << sizeof(short) << endl;
+    cout << "Size of int: " << sizeof(int) << endl;
+    cout << "Size of long: " << sizeof(long) << endl;
+    cout << "Size of long long: " << sizeof(long long) << endl;
+    return 0;
+}
