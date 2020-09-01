c3afdeaad9ae4cbd48f0cc9334bdb8536cb40d37
吴翱翔 <pylint@yandex.com>
2020-09-01 21:45:38 +0800

    AC了Add binary一题

--- leetcode/easy/add_bigint_string.cpp
+++ leetcode/easy/add_bigint_string.cpp
@@ -7,7 +7,7 @@ class Solution {
 public:
     static string addStrings(string num1, string num2) {
         string res;
-        int i = (int)num1.size()-1, j = (int)num2.size()-1;
+        int i = (int) num1.size() - 1, j = (int) num2.size() - 1;
         int carry = 0;
         while (i >= 0 || j >= 0 || carry > 0) {
             int x = i >= 0 ? num1[i] - '0' : 0;
@@ -15,6 +15,22 @@ public:
             int curr_digit = x + y + carry;
             carry = curr_digit / 10;
             res.push_back((curr_digit % 10) + '0');
+            i--;
+            j--;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+
+    static string addBinary(string a, string b) {
+        string res;
+        int i = (int) a.size() - 1, j = (int) b.size() - 1;
+        int carry = 0;
+        while (i >= 0 || j >= 0 || carry > 0) {
+            carry += i >= 0 ? a.at(i) == '1' : 0;
+            carry += j >= 0 ? b.at(j) == '1' : 0;
+            res.push_back((carry % 2 == 0) ? '0' : '1');
+            carry /= 2;
             i--;
             j--;
         }
