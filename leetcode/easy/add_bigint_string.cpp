47b4d10b447c1ba149cebbb19ca9170ed089e5ce
吴翱翔 <pylint@yandex.com>
2020-09-01 21:07:14 +0800

    临摹了长整数加法官方解法

--- leetcode/easy/add_bigint_string.cpp
+++ leetcode/easy/add_bigint_string.cpp
@@ -7,35 +7,17 @@ class Solution {
 public:
     static string addStrings(string num1, string num2) {
         string res;
-        const int m = num1.size();
-        const int n = num2.size();
-        const int min_m_n = min(m, n);
-        const int max_m_n = max(m, n);
-        const int max_minus_min = max_m_n - min_m_n;
+        int i = (int)num1.size()-1, j = (int)num2.size()-1;
         int carry = 0;
-        for (size_t i = max_m_n; i>max_minus_min; i--) {
-            int digit = (num1[i] - '0') + (num2[i] - '0') + carry;
-            res.push_back((digit % 10) + '0');
-            carry = digit / 10;
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int x = i >= 0 ? num1[i] - '0' : 0;
+            int y = j >= 0 ? num2[j] - '0' : 0;
+            int curr_digit = x + y + carry;
+            carry = curr_digit / 10;
+            res.push_back((curr_digit % 10) + '0');
+            i--;
+            j--;
         }
-
-        if (m > n) {
-            for (size_t i = m-min_m_n; i>0; i--) {
-                int digit = (num1[i] - '0') + carry;
-                res.push_back((digit % 10) + '0');
-                carry = digit / 10;
-            }
-        } else if (m < n) {
-            for (size_t i = n-min_m_n; i>0; i--) {
-                int digit = (num2[i] - '0') + carry;
-                res.push_back((digit % 10) + '0');
-                carry = digit / 10;
-            }
-        }
-        if (carry > 0) {
-            res.push_back(carry + '0');
-        }
-
         reverse(res.begin(), res.end());
         return res;
     }
@@ -43,8 +25,8 @@ public:
 
 int main() {
     cout << Solution::addStrings("9", "98") << endl;
-//    cout << Solution::addStrings("789", "654") << endl;
-//    cout << Solution::addStrings("1", "1") << endl;
+    // 1443
+    cout << Solution::addStrings("789", "654") << endl;
     return 0;
 }
 
