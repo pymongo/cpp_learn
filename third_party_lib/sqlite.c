0bb030025e357e50b96bde10fe6624fe398e1a05
wuaoxiang <os.popen@gmail.com>
2021-07-20 17:07:40 +0800

    move some code to other rust repo

--- third_party_lib/sqlite.c
+++
@@ -1,8 +0,0 @@
-#include <sqlite3.h>
-#include <stdio.h>
-
-// check libsqlite3-dev whether install: ldconfig -p | grep libsqlite3
-int main() {
-    printf("sqlite3_dylib_version = %s\n", sqlite3_libversion());
-    return 0;
-}
